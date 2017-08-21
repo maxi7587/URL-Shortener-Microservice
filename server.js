935294bf7f2deb6e3f8cedc27d9b9f6edfd0e9b1
Maxi <maxi7587@hotmail.com>
2017-08-21 01:06:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ app.get("/new/:data", function (request, response) {
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('App is listening on port ' + listener.address().port);
 });
   
